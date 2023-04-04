09d66a08b42dd5f990cff8c02fed6f9e84d481c5
tracy <77692393+tracympham@users.noreply.github.com>
2023-04-04 12:23:51 -0700

    changed theme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -96,7 +96,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'valentine',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
